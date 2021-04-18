4488af023a86b6138080ad090fdaa6ad5aadc1c7
CyberXw <ahmetcandan1@outlook.com.tr>
2021-04-18 12:07:23 +0300

    entity framework altyapısı hazırlandı

---
+++ DataAccess/Abstract/IEntityRepository .cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IEntityRepository<T>
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter =null);// Generic bir IEntityRepository oluşturduk
+
+        T Get();
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+        
+
+    }
+}
