3b85fab5fc529c2dd4490f211c75ce2d94dd9a6a
CyberXw <ahmetcandan1@outlook.com.tr>
2021-05-03 14:10:57 +0300

    WebAPI hazırlanıyor

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -19,7 +19,7 @@ namespace ConsoleUI
             var result = carManager.GetCarDetails();
 
             if (result.Success == true)
-            {
+            { 
                 foreach (var car in result.Data)
                 {
                     Console.WriteLine(car.DailiyPrice + "/" + car.BrandName);
---
+++ WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,38 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityRepository;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] //Attribute
+    public class CarsController : ControllerBase
+    {
+
+        //naming convension kurallarına uyuyoruz
+        // Losely coupled ile bağımlılığımızı azalttık
+        // IoC Container -- Inversion of Control(değişimin kontrolü) ==> bellekte bir kutu oluşturur ve içine referanslar koyuyoruz
+        ICarService _carService;
+
+        public CarsController(ICarService carService)
+        {
+            _carService = carService;
+        }
+
+        [HttpGet]
+        public List<Car> Get()
+        {
+            //Dependency chain 
+            ICarService carService = new CarManager(new EfCarDal());
+            var result = _carService.GetAll();
+            return result.Data;
+        }
+    }
+}
