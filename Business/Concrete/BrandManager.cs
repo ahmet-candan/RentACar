6bdeb974f3fbbed00605c19e2047d968baefcc3e
CyberXw <ahmetcandan1@outlook.com.tr>
2021-05-05 02:58:11 +0300

    business katmanı gelişiyor

---
+++ Business/Concrete/BrandManager.cs
@@ -0,0 +1,49 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class BrandManager : IBrandService
+    {
+
+        IBrandDal _brandDal;
+
+        public BrandManager(IBrandDal brandDal)
+        {
+            _brandDal = brandDal;
+        }
+
+        public IResult Add(Brand brand)
+        {
+            _brandDal.Add(brand);
+            return new SuccessResult(Messages.BrandAddedMessage);
+        }
+
+        public IResult Delete(Brand brand)
+        {
+            _brandDal.Delete(brand);
+            return new SuccessResult(Messages.BrandDeletedMessage);
+        }
+
+        public IDataResult<List<Brand>> GetAll()
+        {
+            return new SuccessDataResult<List<Brand>>(_brandDal.GetAll(), Messages.BrandAllListedMessage);
+        }
+
+        public IDataResult<Brand> GetById(int brandId)
+        {
+            return new SuccessDataResult<Brand>(_brandDal.Get(b => b.Id == brandId), Messages.BrandSingularFilteredListedMessage);
+        }
+
+        public IResult Update(Brand brand)
+        {
+            return new SuccessResult(Messages.BrandUpdatedMessage);
+        }
+    }
+}
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,5 +10,11 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         internal static string MaintenanceTime = "Sistem bakımda!";
         internal static string ProductsListed = "Ürünler listelendi";
+
+        public static string BrandAddedMessage = "Marka eklendi";
+        internal static string BrandDeletedMessage = "Marka silindi";
+        internal static string BrandAllListedMessage = "Tüm markalar listelendi";
+        internal static string BrandSingularFilteredListedMessage = "Id ye göre maraklar listelendi";
+        internal static string BrandUpdatedMessage = "Marka güncellendi";
     }
 }
